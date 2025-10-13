ca2a9e4de531a534c9f0373265b8c4c8f117f9c9
jazzyboi1991 <funkybot2002@gmail.com>
2025-10-13 15:02:20 +0900

    Log-in

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
+const loginForm = document.querySelector("#login-form");
 const loginInput = document.querySelector("#login-form input");
-const loginButton = document.querySelector("#login-form button");
 
-function handleLoginBtnClick() {
-    const username = loginInput.value;
+function onLoginSubmit(event) {
+    event.preventDefault();
+    console.log(loginInput.value);
 }
 
-loginButton.addEventListener("click", handleLoginBtnClick);
+loginForm.addEventListener("submit", onLoginSubmit);
